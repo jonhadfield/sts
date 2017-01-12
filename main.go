3b79a7f59aeebc6d643d57c97cf9a514c9af58b5
Jon Hadfield <jon@lessknown.co.uk>
2017-01-12 17:54:30 +0000

    get session token working.

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/aws/aws-sdk-go/aws"
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func main() {
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Println("failed to create session,", err)
+		return
+	}
+
+	// Try to load the MFA device serial from environment variable
+	envMFADevice := os.Getenv("AWS_MFA_DEVICE")
+	var serialNo string
+	if envMFADevice == "" {
+		fmt.Print("Enter serial number: ")
+		fmt.Scanln(&serialNo)
+	} else {
+		serialNo = envMFADevice
+	}
+	var tokenVal string
+	fmt.Print("Enter token value: ")
+	fmt.Scanln(&tokenVal)
+	svc := sts.New(sess)
+
+	params := &sts.GetSessionTokenInput{
+		DurationSeconds: aws.Int64(3600),
+		SerialNumber:    &serialNo,
+		TokenCode:       &tokenVal,
+	}
+	resp, err := svc.GetSessionToken(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Credentials expire: %s\n", *resp.Credentials.Expiration)
+
+	// Set EnvVars
+	os.Setenv("AWS_ACCESS_KEY_ID", *resp.Credentials.AccessKeyId)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", *resp.Credentials.SecretAccessKey)
+	os.Setenv("AWS_SECURITY_TOKEN", *resp.Credentials.SessionToken)
+	syscall.Exec(os.Getenv("SHELL"), []string{os.Getenv("SHELL")}, syscall.Environ())
+}
