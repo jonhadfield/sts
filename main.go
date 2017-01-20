483904b6d322cc476e114658448a6174d34df289
Jon Hadfield <jon@lessknown.co.uk>
2017-01-20 15:23:15 +0000

    add basic command line options for get-session-token and give options for forking to a shell and for hiding credential output.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/urfave/cli"
 	"os"
@@ -16,15 +18,20 @@ type cliArgs struct {
 	displayOnly bool
 }
 
-func getMFASerial() (serialNo string) {
+func getMFASerial(sess client.ConfigProvider) (serialNo string) {
 	// Try to load the MFA device serial using long term credentials
+	svc := iam.New(sess)
+	params := &iam.ListMFADevicesInput{}
+	resp, err := svc.ListMFADevices(params)
+	if err == nil {
+		return *resp.MFADevices[0].SerialNumber
 
+	}
 	// Try to load the MFA device serial from environment variable
 	envMFADevice := os.Getenv("AWS_MFA_DEVICE")
 	if envMFADevice == "" {
 		os.Getenv("MFA_DEVICE")
 	}
-	//var serialNo string
 	if envMFADevice == "" {
 		fmt.Print("Enter serial number: ")
 		fmt.Scanln(&serialNo)
@@ -34,18 +41,13 @@ func getMFASerial() (serialNo string) {
 	return serialNo
 }
 
-func getSessionToken(display bool, shell bool) {
-	sess, err := session.NewSession()
-	if err != nil {
-		fmt.Println("failed to create session,", err)
-		return
-	}
-
+func getSessionToken(sess client.ConfigProvider, hide bool, shell bool) {
+	// fmt.Println("Getting MFA Serial...")
+	serialNo := getMFASerial(sess)
 	var tokenVal string
 	fmt.Print("Enter token value: ")
 	fmt.Scanln(&tokenVal)
 	svc := sts.New(sess)
-	serialNo := getMFASerial()
 	params := &sts.GetSessionTokenInput{
 		DurationSeconds: aws.Int64(3600),
 		SerialNumber:    &serialNo,
@@ -58,23 +60,21 @@ func getSessionToken(display bool, shell bool) {
 		return
 	}
 
-	fmt.Printf("Credentials expire: %s\n", *resp.Credentials.Expiration)
-
-	// Set EnvVars
-	os.Setenv("AWS_ACCESS_KEY_ID", *resp.Credentials.AccessKeyId)
-	os.Setenv("AWS_SECRET_ACCESS_KEY", *resp.Credentials.SecretAccessKey)
-	os.Setenv("AWS_SECURITY_TOKEN", *resp.Credentials.SessionToken)
-
-	fmt.Println("\n===========")
-	fmt.Println("\nCREDENTIALS")
-	fmt.Println("\n===========")
-	fmt.Printf("AccessKeyId: %s\n", *resp.Credentials.AccessKeyId)
-	fmt.Printf("SecretAccessKey: %s\n", *resp.Credentials.SecretAccessKey)
-	fmt.Printf("SessionToken: %s\n", *resp.Credentials.SessionToken)
-	fmt.Printf("Expiration: %s\n", *resp.Credentials.Expiration)
-
+	if hide != true {
+		fmt.Println("\n===========")
+		fmt.Println("CREDENTIALS")
+		fmt.Println("===========")
+		fmt.Printf("AccessKeyId: %s\n", *resp.Credentials.AccessKeyId)
+		fmt.Printf("SecretAccessKey: %s\n", *resp.Credentials.SecretAccessKey)
+		fmt.Printf("SessionToken: %s\n", *resp.Credentials.SessionToken)
+		fmt.Printf("Expiration: %s\n", *resp.Credentials.Expiration)
+	}
 	if shell == true {
-		fmt.Println("Launching new shell with temporary credentials...")
+		// Set environment variables and fork
+		os.Setenv("AWS_ACCESS_KEY_ID", *resp.Credentials.AccessKeyId)
+		os.Setenv("AWS_SECRET_ACCESS_KEY", *resp.Credentials.SecretAccessKey)
+		os.Setenv("AWS_SECURITY_TOKEN", *resp.Credentials.SessionToken)
+		fmt.Println("\nLaunching new shell with temporary credentials...")
 		syscall.Exec(os.Getenv("SHELL"), []string{os.Getenv("SHELL")}, syscall.Environ())
 	}
 }
@@ -105,14 +105,20 @@ func main() {
 	//	},
 	//}
 
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Println("failed to create session,", err)
+		return
+	}
+
 	app.Commands = []cli.Command{
 		{
 			Name:    "get-session-token",
 			Aliases: []string{"st"},
 			Usage:   "get a session token",
 			Flags: []cli.Flag{
-				cli.BoolTFlag{
-					Name:  "hide, d",
+				cli.BoolFlag{
+					Name:  "hide",
 					Usage: "hide credentials",
 				},
 				cli.BoolFlag{
@@ -121,10 +127,10 @@ func main() {
 				},
 			},
 			Action: func(c *cli.Context) error {
-				fmt.Println(app.Flags)
-				fmt.Println(c.Args())
-				fmt.Println(c.Bool("display"))
-				getSessionToken(c.Bool("display"), c.Bool("shell"))
+				// fmt.Println(app.Flags)
+				// fmt.Println(c.Args())
+				// fmt.Println(c.Bool("display"))
+				getSessionToken(sess, c.Bool("hide"), c.Bool("shell"))
 				return nil
 			},
 		},
